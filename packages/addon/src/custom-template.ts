b3ffb6df5a6aec1a9b92f5faaddfcd6d4741868f
panteLx <info@pantelx.com>
2025-05-30 02:03:28 +0200

    feat: add Tawk.to live chat script to landing template

--- packages/addon/src/custom-template.ts
+++ packages/addon/src/custom-template.ts
@@ -1076,6 +1076,19 @@ function landingTemplate(manifest: Manifest): string {
       }, 2000);
     });
   </script>
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/6838da7b18e960190e216bec/1isf1asmt';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  </script>
+  <!--End of Tawk.to Script-->
 </body>
 </html>
 `;
